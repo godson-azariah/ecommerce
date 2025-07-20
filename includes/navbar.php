bc43a8dbec949bfc5e39949f7a9f81f1c0d7b5f2
godson-azariah <godson.azh@gmail.com>
2025-07-20 19:28:58 +0530

    product delete and edit feature is added

--- includes/navbar.php
+++ includes/navbar.php
@@ -12,8 +12,6 @@
                     <li class="nav-item"><a class="nav-link" href="cart.php">Cart</a></li>
                     <li class="nav-item"><a class="nav-link" href="checkout.php">Checkout</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-                    <li class="nav-item"><a class="nav-link" href="delete-product.php">delete-product</a></li>
-                    <li class="nav-item"><a class="nav-link" href="edit-product.php">edit-product</a></li>
                     <li class="nav-item"><a class="nav-link" href="manage-product.php">manage-product</a></li>
                     <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
                     <?php 
